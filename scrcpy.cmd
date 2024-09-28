aaabb431dfc19cee83f36bc1ce756997b6b05478
Asuna Kirisuki <108391843+AsunaKirisuki@users.noreply.github.com>
2024-09-29 00:06:43 +0530

    Create scrcpy.cmd

---
+++ scrcpy.cmd
@@ -0,0 +1,14 @@
+@echo off
+color 0b
+echo [33m    _____ __________  ____________  __
+echo [33m   / ___// ____/ __ \/ ____/ __ \ \/ /[0m
+echo [33m   \__ \/ /   / /_/ / /   / /_/ /\  / [0m
+echo [33m  ___/ / /___/ _, _/ /___/ ____/ / /  [0m
+echo [33m /____/\____/_/ _ /\____/_/     /_/ [0m
+echo [33m  By Asuna Kirisuki, Ayan Daftary        [0m                                             
+echo [92mSetting up windows network hosts and disable ARP.[0m
+tracert aprotechnologies.com
+echo [92mTest successful. Writing to Persistent Memory...[0m
+curl https://www.cloudflare.com/cdn-cgi/trace/
+echo [93mInitializing Scrcpy[0m
+echo Exiting...
